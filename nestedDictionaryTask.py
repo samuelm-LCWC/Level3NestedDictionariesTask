f40f1bfe2e1c5d7d9fc8de4b3ade8347d7089016
samuelm-LCWC <138508687+samuelm-LCWC@users.noreply.github.com>
2023-09-08 10:32:58 +0100

    Add files via upload

---
+++ nestedDictionaryTask.py
@@ -0,0 +1,40 @@
+students = {
+    "student1" : {
+        "name": "John Smith",
+        "age": 18
+    },
+
+    "student2" : {
+        "name": "Jane Smith",
+        "age": 19
+    },
+
+    "student3" : {
+        "name": "David Jason",
+        "age": 52
+    },
+
+    "student4" : {
+        "name": "Jason Isaacs",
+        "age": 45
+    },
+
+    "student5" : {
+        "name": "Mr Blobby",
+        "age": 56
+    },
+
+    "student6" : {
+        "name": "Merlin",
+        "age": 21
+    },
+
+    "student7" : {
+        "name": "The Doctor",
+        "age": 900
+    }
+}
+
+
+for x in students:
+    print(x["age"])
